2c1ea3018a1f5a7dfd84e5773f68633d064522b4
Maksim <maxim.fizh@gmail.com>
2023-11-22 14:42:16 +0400

    Refactor + Add #12

---
+++ ForLeetCode/Problems/1-TwoSumm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ForLeetCode
+{
+    public static class TwoSumm
+    {
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> seen = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (seen.ContainsKey(target - nums[i]))
+                {
+                    return new int[] { seen[target - nums[i]], i };
+                }
+
+                if (!seen.ContainsKey(nums[i]))
+                {
+                    seen.Add(nums[i], i);
+                }
+
+            }
+            return null;
+        }
+    }
+}
---
+++ ForLeetCode/Problems/9-PalindromeInteger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Problem9
+{
+    public static bool IsPalindrome(int x)
+    {
+
+        if (x < 0) return false;
+        if (x < 10) return true;
+
+        var stringValue = x.ToString();
+
+        for (x = 0; x < stringValue.Length / 2; x++)
+        {
+            if (stringValue[x] != stringValue[stringValue.Length - 1 - x]) return false;
+        }
+
+        return true;
+    }
+
+    public static Problem9 operator + (Problem9 left, Problem9 right)
+    {
+        return new Problem9();
+    }
+}
